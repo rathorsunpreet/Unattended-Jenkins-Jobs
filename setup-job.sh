9764e65c29bed6c96a01e31fcefa5156c7de218e
Sunpreet Singh Rathor <rathorsunpreet@gmail.com>
2024-06-03 13:38:11 -0400

    Added checks in shell script

--- setup-job.sh
+++ setup-job.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-java -jar jenkins-cli.jar -auth "$1":"$2" -s http://localhost:8080/ create-job simple-api < simple-api.xml
-java -jar jenkins-cli.jar -auth "$1":"$2" -s http://localhost:8080/ build simple-api
+if [ "$#" -eq 0 ]
+then
+    echo "No arguments supplied!"
+    exit 1
+elif [ "$#" -ne 2 ]
+then
+    echo "Incorrect count of arguments!"
+    exit 1
+else
+    echo "Creating a Job"
+    java -jar jenkins-cli.jar -auth "$1":"$2" -s http://localhost:8080/ create-job simple-api < simple-api.xml
+    echo "Running created Job"
+    java -jar jenkins-cli.jar -auth "$1":"$2" -s http://localhost:8080/ build simple-api
+fi
