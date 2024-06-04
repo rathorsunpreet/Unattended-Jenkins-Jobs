db74627719b1aeeca8f28971eb8d9590b107657c
Sunpreet Singh Rathor <rathorsunpreet@gmail.com>
2024-06-04 01:04:35 -0400

    Updated shell script to use flags

--- setup-job.sh
+++ setup-job.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
 
-if [ "$#" -eq 0 ]
-then
-    echo "No arguments supplied!"
+# Set the flags to use and other variables
+flags=':u:p:h'
+usrname=''
+psswd=''
+
+# Help Message
+usage() {
+    echo "Usage: $0 [-u username] [-p password] [-h (help)]" 1>&2
+}
+
+exit_on_fail() {
+    usage
     exit 1
-elif [ "$#" -ne 2 ]
+}
+
+# Cycle through the flags and assign the arguments to appropriate variables
+# If no flags or no arguments, then take action
+while getopts "${flags}" options; do
+    case "${options}" in
+        u)
+            usrname=${OPTARG}
+            ;;
+        p)
+            psswd=${OPTARG}
+            ;;
+        h)
+            exit_on_fail
+            ;;
+        :)
+            echo "Error: -${OPTARG} requires an argument."
+            exit_on_fail
+            ;;
+        *)
+            exit_on_fail
+            ;;
+    esac
+    noargs='true'
+done
+
+
+# Make sure both username and password are present
+if [[ -z "${usrname}" || -z "${psswd}" ]]
 then
-    echo "Incorrect count of arguments!"
-    exit 1
-else
-    echo "Creating a Job"
-    java -jar jenkins-cli.jar -auth "$1":"$2" -s http://localhost:8080/ create-job simple-api < simple-api.xml
-    echo "Running created Job"
-    java -jar jenkins-cli.jar -auth "$1":"$2" -s http://localhost:8080/ build simple-api
+    exit_on_fail
 fi
+
+# At this point, both variables are set
+echo "Creating a Job"
+java -jar jenkins-cli.jar -auth "$usrname":"$psswd" -s http://localhost:8080/ create-job simple-api < simple-api.xml
+echo "Running created Job"
+java -jar jenkins-cli.jar -auth "$usrname":"$psswd" -s http://localhost:8080/ build simple-api
